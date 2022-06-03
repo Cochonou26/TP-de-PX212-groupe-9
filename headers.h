cc5b7465e45df5ad2116e5ff5cdb7088eac67ed5
holdenrEsisar <106807459+holdenrEsisar@users.noreply.github.com>
2022-06-03 14:52:06 +0200

    Update headers.h

--- headers.h
+++ headers.h
@@ -1,12 +1,18 @@
-char *[][] lectureTableau(int niveau);
-void afficheTableau(char *niveau[][]);
-//char gestionEntree(?);
-char verifDeplacement(char *niveau[][]);
-void deplacement (char *niveau[][]);
-tableauSuivant();
-recommencerNiveau();
-comptage();
-annulerDeplacement();
-sauvegarder();
-enregistrerSolution();
+typedef struct deplacement{
+	int *nbMouvements;
+	int *nbPoussees
+	char *mouvement;
+	char *poussee;
+	deplacement *next;	
+}deplacement;
 
+char *[][] lectureTableau(int niveau); // renvoie le niveau
+void afficheTableau(char *niveau[][]); // affiche le tableau sur le terminal
+//char gestionEntree(?);
+char verifDeplacement(char *niveau[][]); // renvoie un "booléen"
+void deplacement (char *niveau[][]); // modifie le niveau
+char detecReussite(char *niveau[][]); // renvoit un booléen
+void comptage(int *nbMouvements, int *nbPoussees);
+void annulerDeplacement(deplacement *trajet);
+void sauvegarder(FILE *fichierSauvegarde);
+void enregistrerSolution(FILE *fichierSolutions);
