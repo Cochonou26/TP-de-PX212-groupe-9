54695ed2e762ff40e30c587679686e97bfd0cc0f
holdenrEsisar <106807459+holdenrEsisar@users.noreply.github.com>
2022-06-03 14:21:53 +0200

    Add files via upload

---
+++ headers.h
@@ -0,0 +1,12 @@
+char *[][] lectureTableau(int niveau);
+void afficheTableau(char *niveau[][]);
+//char gestionEntree(?);
+char verifDeplacement(char *niveau[][]);
+void deplacement (char *niveau[][]);
+tableauSuivant();
+recommencerNiveau();
+comptage();
+annulerDeplacement();
+sauvegarder();
+enregistrerSolution();
+
