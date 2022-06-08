944ad006e1136f6e22809631f40566a931a96371
Cochonou26 <106807194+Cochonou26@users.noreply.github.com>
2022-06-08 15:58:41 +0200

    Delete sauvegarde.h

--- sauvegarde.h
+++
@@ -1,5 +0,0 @@
-int repriseSauvegarde(Dep *pile, Niveau *pNiveau);
-int verifierSauvegarde();
-int sauvegarderTableau(Dep *pile, int numNiveau);
-int enregistrement(Dep *pile, int numNiveau);
-long chercherNiveau();
