3f077ac73d649e5b9906b6497daba1ae6b235da3
Cochonou26 <106807194+Cochonou26@users.noreply.github.com>
2022-06-08 23:36:14 +0200

    Add files via upload

---
+++ newDeplacement/sauvegarde.h
@@ -0,0 +1,5 @@
+int repriseSauvegarde(Dep *pile, Niveau *pNiveau, int *compteurCoups);
+int verifierSauvegarde();
+int sauvegarderTableau(Dep *pile, int numNiveau);
+int enregistrement(Dep *pile, int numNiveau);
+int chercherNiveau();
