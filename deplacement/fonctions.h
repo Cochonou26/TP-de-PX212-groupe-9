d641316a258107894316d2daf9a556c50c2891e1
Nucrino <106820700+Nucrino@users.noreply.github.com>
2022-06-08 13:39:01 +0200

    Delete deplacement directory

--- deplacement/fonctions.h
+++
@@ -1,8 +0,0 @@
-#include "util.h"
-
-void afficherNiveau(char ***tableauNiveau, int nLignes, int nColonnes);
-int deplacement (niveau *pNiveau, char dep);
-niveau *creerNiveau(char *fichierNiveaux, int numNiveau);
-int configureTerminal();
-int litClavier();
-
