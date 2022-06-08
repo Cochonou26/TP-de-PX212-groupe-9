844be4ce8fb5342c873c9aa144575851ec0d0c37
Cochonou26 <106807194+Cochonou26@users.noreply.github.com>
2022-06-08 23:35:55 +0200

    Delete sauvegarde.h

--- newDeplacement/sauvegarde.h
+++
@@ -1,5 +0,0 @@
-int repriseSauvegarde(Dep *pile, Niveau *pNiveau, *compteurCoups);
-int verifierSauvegarde();
-int sauvegarderTableau(Dep *pile, int numNiveau);
-int enregistrement(Dep *pile, int numNiveau);
-int chercherNiveau();
