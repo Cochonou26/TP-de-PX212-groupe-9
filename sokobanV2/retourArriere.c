404577e45b6ee9ea2cb2a2375b4f7b85908c021f
Nucrino <106820700+Nucrino@users.noreply.github.com>
2022-06-10 09:27:13 +0200

    Update retourArriere.c

--- sokobanV2/retourArriere.c
+++ sokobanV2/retourArriere.c
@@ -3,6 +3,24 @@
 #include "global.h"
 #include "util.h"
 
+/**
+*@file retourArriere.c
+*@brief Contient les fonctions d'empilage des mouvements et de retour Arrière utilisé dans deplacement.c, qui correspond à un dépilage de la pile.
+*@author Groupe 9
+*/
+
+/**@brief Empile un par un les déplacements du joueur et les poussées de caisses.
+*
+*@param[in] char direc		Char correspondant à la direction du mouvement à empiler.
+*@param[in] char depCaisse		Char correspondant à une poussée ou non de la caisse.
+*
+*@param[out] Dep **pile		Pointeur vers pointeur sur structure Dep, correspond à la pile dans laquelle on empile les mouvements.
+*
+*@return int 	Retourne 0 quand l'empilage est terminé.
+*/
+
+
+
 int empilerDeplacement(char direc,char depCaisse, Dep  **pile) {   //Fonction pour empiler un déplacement dans la pile de type Dep (cours)
 	Dep *deplacement;	
 	deplacement= emalloc(sizeof(Dep));
@@ -12,6 +30,15 @@ int empilerDeplacement(char direc,char depCaisse, Dep  **pile) {   //Fonction po
 	*pile = deplacement;
 	return 0;
 }
+
+/**@brief Dépile le mouvement précédent de la pile passée en argument, et réalise le mouvement inversé.
+*
+*@param[out] Niveau *pNiveau		Pointeur vers structure Niveau permettant de modifier le tableau pour revenir en arrière.
+*@param[out] Dep **pile			Pointeur vers pointeur de structure Dep, permettant de dépiler la pile et récuperer le mouvement précédent.
+*@param[out] int *compteurCoups 	Pointeur vers Int permettant de décrémenter le nombre de coups à chaque retour arrière.
+*
+*@return int	Renvoie un int selon si le retour arrière à été réussit ou non.
+*/
 
 
 int retourArriere(Niveau *pNiveau, Dep **pile, int *compteurCoups) {   //Fonction pour annuler le coup précédent
